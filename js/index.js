9825e13c4a136fd2fc909648468e024dfb5ff211
adrife2005 <adrife2000@hotmail.com>
2023-12-16 12:38:21 -0700

    Updates

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
 // Hamburger-Menu
 
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.querySelector('.hamburger-button');
-    const mobileMenu = document.querySelector('.mobile-menu');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.querySelector(".hamburger-button");
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-    hamburgerMenu.addEventListener('click', () => mobileMenu.classList.toggle('active'));
-})
+  hamburgerMenu.addEventListener("click", () =>
+    mobileMenu.classList.toggle("active")
+  );
+});
 
 // Navbar
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener('scroll', () => {
-        const navbar = document.querySelector('nav');
-        if (window.scrollY > 0) {
-            navbar.classList.remove('p-3');
-            navbar.classList.add('px-3')
-        } else {
-            navbar.classList.add('p-3');
-        }
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("scroll", () => {
+    const navbar = document.querySelector("nav");
+    if (window.scrollY > 0) {
+      navbar.classList.remove("p-3");
+      navbar.classList.add("px-3");
+    } else {
+      navbar.classList.add("p-3");
+    }
+  });
+});
+
+// Test
